c18962d87f867cfd6a682a55d7364c61fee9790d
Benjamin Dionysus <benjamin@sixbynine.com>
2020-12-14 16:04:13 -0600

    not working

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,55 @@
 import './App.css';
 import Test from "./Test"
 import React from "react";
-function App() {
+import { Security, SecureRoute, LoginCallback } from '@okta/okta-react';
+import config from './config';
+import { withAuth } from '@okta/okta-react';
+import { useAuth } from './components/auth';
+
+import { OktaAuth } from '@okta/okta-auth-js';
+import { Route, useHistory, Switch } from 'react-router-dom';
+import CustomLoginComponent from './Login';
+const App = withAuth(({ auth }) => {
+  const [authenticated, user] = useAuth(auth);
+  const history = useHistory(); // example from react-router  
+  const customAuthHandler = () => {
+    // Redirect to the /login page that has a CustomLoginComponent
+    history.push('/login');
+  };
+  
   return (
+  //   <Security
+  //   oktaAuth={OktaAuth}
+  //   onAuthRequired={customAuthHandler}
+  // >
+  <Security
+  issuer={`${process.env.REACT_APP_OKTA_ORG_URL}/oauth2/default`}
+  client_id={process.env.REACT_APP_OKTA_CLIENT_ID}
+  redirect_uri={`${window.location.origin}/implicit/callback`}
+>
+  
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
 
-          <Test />
+        <Route path="/login/callback" component={LoginCallback} />
+          <Route path="/login" component={CustomLoginComponent} />
+          
+          <SecureRoute path="/messages" component={Test} />
         <div>Now we need to implement user authentication</div>
+        
+    {authenticated !== null && (
+      <button
+        onClick={() => authenticated ? auth.logout() : auth.login()}
+        className="App-link"
+      >
+        Log {authenticated ? 'out' : 'in'}
+      </button>
+    )}
       </header>
     </div>
+    </Security>
   );
-}
+});
 
 export default App;
---
+++ client/src/config.js
@@ -0,0 +1,17 @@
+const CLIENT_ID = process.env.CLIENT_ID || '{clientId}';
+const ISSUER = process.env.ISSUER || 'https://{yourOktaDomain}.com/oauth2/default';
+const OKTA_TESTING_DISABLEHTTPSCHECK = process.env.OKTA_TESTING_DISABLEHTTPSCHECK || false;
+
+export default {
+  oidc: {
+    clientId: CLIENT_ID,
+    issuer: ISSUER,
+    redirectUri: 'http://localhost:3001/implicit/callback',
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,
+    disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK,
+  },
+  resourceServer: {
+    messagesUrl: 'http://localhost:3001/api/messages',
+  },
+};
